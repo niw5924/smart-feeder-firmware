5c5cb1c16d4a1a2cec7995a2eba65581ffefc728
niw5924 <niw5924@naver.com>
2025-12-15 21:16:18 +0900

    feat: ESP32 SoftAP 웹서버(설정용) 추가

--- SmartFeeder_IoT_Custom.ino
+++ SmartFeeder_IoT_Custom.ino
@@ -1,3 +1,5 @@
+#include <WiFi.h>
+#include <WebServer.h>
 #include <Servo.h>
 
 const int motor_pin1 = D2;
@@ -6,6 +8,29 @@ const int motor_button = D7;
 
 const int servo_pin = D6;
 Servo servo;
+
+WebServer server(80);
+
+const char* AP_SSID = "SmartFeeder_Setup";
+const char* AP_PASS = "12345678";
+
+void startApAndWeb() {
+  WiFi.mode(WIFI_MODE_AP);
+  WiFi.softAP(AP_SSID, AP_PASS);
+
+  IPAddress ip = WiFi.softAPIP();
+
+  server.on("/", []() {
+    server.send(200, "text/plain", "Hello! SmartFeeder Web is running.");
+  });
+
+  server.begin();
+
+  Serial.print("AP SSID: ");
+  Serial.println(AP_SSID);
+  Serial.print("Open: http://");
+  Serial.println(ip);
+}
 
 void setup() {
   pinMode(motor_pin1, OUTPUT);
@@ -19,9 +44,13 @@ void setup() {
   servo.write(180);
 
   Serial.begin(115200);
+
+  startApAndWeb();
 }
 
 void loop() {
+  server.handleClient();
+
   if (digitalRead(motor_button) == LOW) {
     Serial.println("pressed");
 
